96ac70c243176fe766339454c4709c2323613661
antonianicolae <nicolaeantonia18@stud.ase.ro>
2021-03-04 14:31:04 +0200

    During Second Lab

--- Week2 - Java Recap/src/ro/ase/csie/cts/g1094/laboratory/exceptions/IllegalTransferException.java
+++ Week2 - Java Recap/src/ro/ase/csie/cts/g1094/laboratory/exceptions/IllegalTransferException.java
@@ -1,5 +1,13 @@
 package ro.ase.csie.cts.g1094.laboratory.exceptions;
 
-public class IllegalTransferException {
+public class IllegalTransferException extends Exception {
 
+	public IllegalTransferException(String message) {
+		super(message);
+	}
+	
+	public IllegalTransferException() {
+		
+	}
+	
 }
--- Week2 - Java Recap/src/ro/ase/csie/cts/g1094/laboratory/models/Account.java
+++ Week2 - Java Recap/src/ro/ase/csie/cts/g1094/laboratory/models/Account.java
@@ -1,5 +1,8 @@
 package ro.ase.csie.cts.g1094.laboratory.models;
 
-public class Account {
-
+public abstract class Account {
+	public abstract double getBalance();
+	public abstract void deposit(Object amount);
+	public abstract void withdraw(Object amount);
+	public abstract void transfer(Account destination, Object amount);
 }
