a6ca085d70c4e535f5caec2380869ac563ec428f
antonianicolae <nicolaeantonia18@stud.ase.ro>
2021-03-09 15:31:30 +0200

    Phase 2 - Final Commit

--- Week3/src/ro/ase/csie/cts/g1094/refactor/phase1/Product.java
+++ Week3/src/ro/ase/csie/cts/g1094/refactor/phase1/Product.java
@@ -1,26 +1,38 @@
 package ro.ase.csie.cts.g1094.refactor.phase1;
 
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidAgeException;
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidPriceException;
+
 public class Product {
 	
 	public static final int MAX_AGE_ACCOUNT = 10;
 	public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
 	
-	public float computePriceWithDiscount(ProductType productType, float initialPrice, int accountAge)
+	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeException
 	  {
+		
+		if(price<=0) {
+			throw new InvalidPriceException();
+		}
+		
+		if(accountAge <0) {
+			throw new InvalidAgeException();
+		}
+		
 	    float finalPrice = 0;
 	    float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100; 
 	    switch(productType) {
 	    case NEW:
-	    	finalPrice = initialPrice;
+	    	finalPrice = price;
 	    	break;
 	    case DISCOUNTED:
-	    	 finalPrice = (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice));
+	    	 finalPrice = (price - (ProductType.DISCOUNTED.getDiscount() * price)) - fidelityDiscount * (price - (ProductType.DISCOUNTED.getDiscount() * price));
 	    	 break;
 	    case LIMITED_STOCK:
-	    	 finalPrice = (initialPrice - (ProductType.LIMITED_STOCK.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LIMITED_STOCK.getDiscount() * initialPrice));
+	    	 finalPrice = (price - (ProductType.LIMITED_STOCK.getDiscount() * price)) - fidelityDiscount * (price - (ProductType.LIMITED_STOCK.getDiscount() * price));
 	    	 break;
 	    case LEGACY:
-	    	finalPrice = (initialPrice - (ProductType.LEGACY.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LEGACY.getDiscount() * initialPrice));
+	    	finalPrice = (price - (ProductType.LEGACY.getDiscount() * price)) - fidelityDiscount * (price - (ProductType.LEGACY.getDiscount() * price));
 	    	break;
 	    default:
 	    	throw new UnsupportedOperationException("The enum type is not covered");
