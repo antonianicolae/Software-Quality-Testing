c8d2744a37b44c366a94162e59596242c7de6b02
antonianicolae <nicolaeantonia18@stud.ase.ro>
2021-05-06 15:40:23 +0300

    Strategy and State Implementation

---
+++ Week10 - Proxy, Strategy and State/src/ro/ase/csie/cts/g1094/dp/state/MoveStateInterface.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1094.dp.state;
+
+public interface MoveStateInterface {
+	public void moveAtDifferentLocation();
+}
---
+++ Week10 - Proxy, Strategy and State/src/ro/ase/csie/cts/g1094/dp/state/WoundedState.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.g1094.dp.state;
+
+public class WoundedState implements MoveStateInterface{
+
+	@Override
+	public void moveAtDifferentLocation() {
+		System.out.println("The hero is wounded");
+		
+	}
+
+}
