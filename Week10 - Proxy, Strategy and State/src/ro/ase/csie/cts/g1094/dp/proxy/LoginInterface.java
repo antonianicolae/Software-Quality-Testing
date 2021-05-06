3da2c5d9461dd49a971b30f428b0e72ab7841a8c
antonianicolae <nicolaeantonia18@stud.ase.ro>
2021-05-06 14:33:50 +0300

    Proxy Implementation

---
+++ Week10 - Proxy, Strategy and State/src/ro/ase/csie/cts/g1094/dp/proxy/LoginInterface.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1094.dp.proxy;
+
+public interface LoginInterface {
+	
+	public boolean login(String username, String password);
+	public boolean checkServerStatus();
+}
---
+++ Week10 - Proxy, Strategy and State/src/ro/ase/csie/cts/g1094/dp/proxy/LoginModule.java
@@ -0,0 +1,26 @@
+package ro.ase.csie.cts.g1094.dp.proxy;
+
+public class LoginModule implements LoginInterface {
+
+	String serverIP;
+	String dbConnection;
+	
+	public LoginModule(String serverIP, String dbConnection) {
+		super();
+		this.serverIP = serverIP;
+		this.dbConnection = dbConnection;
+	}
+	
+	@Override
+	public boolean login(String username, String password) {
+		System.out.println("Connecting to the database: " + dbConnection);
+		if(username.toLowerCase().equals("admin") && password.equals("1234admin")) 
+			return true;
+		else
+			return false;
+	}
+	@Override
+	public boolean checkServerStatus() {
+		return true;
+	}
+}
