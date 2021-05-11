ac380a43a0b54d7ba03934ae750504ea2d6edbe7
antonianicolae <nicolaeantonia18@stud.ase.ro>
2021-05-11 15:20:20 +0300

    Command Implementation

---
+++ Week11 - Chain and Command/src/ro/ase/csie/cts/g1094/dp/command/AsyncTaskInterface.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1094.dp.command;
+
+public interface AsyncTaskInterface {
+	
+	public void awaitTask();
+	
+	
+}
---
+++ Week11 - Chain and Command/src/ro/ase/csie/cts/g1094/dp/command/GameModuleInterface.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1094.dp.command;
+
+public interface GameModuleInterface {
+	public void doTask(String taskName);
+}
